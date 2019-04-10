03afb8e843b6cc46682f1ad87b6b15c14821552f
marita30 <marita.jose.ch@gmail.com>
2019-04-10 17:02:36 -0600

    Creating switch to turn off PSM (Ahorro de energia) excercise 5

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,32 +1,54 @@
 'use strict';
 
 function Thermostat(){
+  this.MAX_LIMIT_PSM_ON = 25;
+  this.MAX_LIMIT_PSM_OFF = 32;
   this.MINIMUM_TEMPERATURE = 10;
   this.powerSavingMode = true;
   this.temperature = 20;
 
 }
-Thermostat.prototype.getCurrentTemperature = function () {
+Thermostat.prototype.getCurrentTemperature = function() {
   return this.temperature;
 };
-Thermostat.prototype.up = function () {
+
+Thermostat.prototype.up = function() {
   this.temperature += 1;
 };
-Thermostat.prototype.down = function () {
+
+Thermostat.prototype.down = function() {
   if (this.isMinimumTemperature()) {
     return;
   }
   this.temperature -= 1;
 };
-Thermostat.prototype.isMinimumTemperature = function () {
+
+Thermostat.prototype.isMinimumTemperature = function() {
   return this.temperature === this.MINIMUM_TEMPERATURE;
 };
-Thermostat.prototype.isPowerSavingModeOn = function () {
+
+Thermostat.prototype.isPowerSavingModeOn = function() {
   return this.powerSavingMode === true;
 };
-Thermostat.prototype.switchPowerSavingModeOff = function () {
+
+Thermostat.prototype.switchPowerSavingModeOff = function() {
   this.powerSavingMode = false;
 };
-Thermostat.prototype.switchPowerSavingModeOn = function () {
+
+Thermostat.prototype.switchPowerSavingModeOn = function() {
   this.powerSavingMode = true;
+};
+
+Thermostat.prototype.up = function() {
+  if (this.isMaximumTemperature()){
+    return;
+  }
+  this.temperature += 1;
+};
+
+Thermostat.prototype.isMaximumTemperature = function() {
+  if (this.isPowerSavingModeOn() === false){
+    return this.temperature === this.MAX_LIMIT_PSM_OFF;
+  }
+  return this.temperature === this.MAX_LIMIT_PSM_ON;
 };
