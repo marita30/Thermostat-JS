e1ace70739a8f9f2aa507168bc994a64fd2e2cea
marita30 <marita.jose.ch@gmail.com>
2019-04-11 17:38:27 -0600

    fixing bugs, excersice 6!

---
+++ src/interface.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  var thermostat = new Thermostat();
+  updateTemperature();
+
+  $('#temp-up').click(function() {
+    thermostat.up();
+    updateTemperature();
+  });
+
+  $('#temp-down').click(function() {
+    thermostat.down();
+    updateTemperature();
+  });
+
+  $('#temp-reset').click(function() {
+    thermostat.reset();
+    updateTemperature();
+  });
+
+  $('#psm-on').click(function() {
+    thermostat.powerSavingModeOn();
+    $('#power-saving').text('on')
+    updateTemperature();
+  })
+
+  $('#psm-off').click(function() {
+    thermostat.powerSavingModeOff();
+    $('#power-saving').text('off')
+    updateTemperature();
+  })
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.getCurrentTemperature());
+    $('#temperature').attr('class', thermostat.energyUsage());
+  }
+
+});
